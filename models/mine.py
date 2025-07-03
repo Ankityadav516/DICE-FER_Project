cda86b9c467d1f055b2b780bfb1f224d498437aa
Ankityadav516 <ankit525p@gmail.com>
2025-07-03 20:46:01 +0530

    modified

--- models/mine.py
+++ models/mine.py
@@ -5,10 +5,11 @@ class MINE(nn.Module):
     def __init__(self, input_dim):
         super(MINE, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(input_dim * 2, 512),
+            nn.Linear(256, 512),  # 128 (z1) + 128 (z2) = 256
             nn.ReLU(),
             nn.Linear(512, 1)
         )
+
 
     def forward(self, x, y):
         return self.net(torch.cat([x, y], dim=1))
