cd6318497572af36b7b95069fa46f2604490ef14
Ankityadav516 <ankit525p@gmail.com>
2025-07-03 21:27:59 +0530

    modified

--- datasets/fer_loader.py
+++ datasets/fer_loader.py
@@ -5,8 +5,14 @@ import torch
 class FERDataset(Dataset):
     def __init__(self, image_paths, labels, transform=None):
         self.image_paths = image_paths
-        self.labels = labels
         self.transform = transform
+
+        # Step 1: Get unique string labels and map to integer IDs
+        unique_labels = sorted(set(labels))
+        self.label2idx = {label: idx for idx, label in enumerate(unique_labels)}
+
+        # Step 2: Convert all string labels to integer indices
+        self.labels = [self.label2idx[label] for label in labels]
 
     def __len__(self):
         return len(self.image_paths)
